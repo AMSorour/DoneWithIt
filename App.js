bcb577d12c1e87c21dec93ab698197dce58a1b6f
AMSorour <54924721+AMSorour@users.noreply.github.com>
2020-05-30 21:12:00 -0400

    Second Commit

--- App.js
+++ App.js
@@ -1,51 +1,38 @@
-
 import * as React from 'react';
-import { Text, View,StyleSheet, Button } from 'react-native'
-import {createStackNavigator} from '@react-navigation/stack'
-import { NavigationContainer } from '@react-navigation/native';
+import {
+  Dimensions,
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Image,
+  Button,
+  TouchableHighlight,
+  Alert,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
 
-
-
-const HomeScreen = ({navigation}) => {
-  return(
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-        <Text>Home Screen</Text>
-        <Button title='Details Page' onPress={() => navigation.navigate("Details")}/>
-    </View>
-  );
-};
-
-const DetailsScreen = ({navigation}) => {
-  return(
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-        <Text>Details Screen</Text>
-    </View>
-  );
-};
-
-const Stack = createStackNavigator();
-
-const App = () => {
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={[styles.container]}>
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: '100%',
+          height: '30%',
+        }}
+      />
+    </SafeAreaView>
   );
-};
-
-export default App;
-
+}
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center',
-   
-  }
-
-})
-
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+});
